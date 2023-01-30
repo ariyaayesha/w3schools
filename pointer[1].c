1d0fdda5ddca6e9ea13063e410dc1dfaba729841
Ayesha Akter <113846939+ariyaayesha@users.noreply.github.com>
2023-01-30 22:04:14 +0600

    Add files via upload

---
+++ pointer[1].c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+int myAge = 43;    
+int* ptr = &myAge;  
+
+printf("%d\n", myAge);
+printf("%p\n", &myAge);
+printf("%p\n", ptr);
+
+return 0;
+
+}
+    
---
+++ string_loop[1].c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+char name[] = "ayesha";
+int i;
+
+for (i = 0; i < 6; ++i) {
+  printf("%c\n",name[i]);
+}
+return 0;
+
+}
+    
